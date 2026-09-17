0a6719901b360b5bb0e5bd81bc7d30d67407b8cf
drkevorkian <sirus.softs@gmail.com>
2026-09-17 09:32:28 -0600

    Require active dispatch failure provenance clearing

--- background-wrapper.js
+++ background-wrapper.js
@@ -72,6 +72,7 @@ if (
   globalThis.__AI_BRIDGE_VIEWPOINT_RUNTIME_V1__?.revalidatesIdentityAtDispatch !== true ||
   globalThis.__AI_BRIDGE_VIEWPOINT_RUNTIME_V1__?.cancelsQueuedOnTabClose !== true ||
   globalThis.__AI_BRIDGE_VIEWPOINT_RUNTIME_V1__?.restoresQueuedSendsAfterWorkerRestart !== false ||
+  globalThis.__AI_BRIDGE_VIEWPOINT_RUNTIME_V1__?.clearsTransientIdentityOnDispatchFailure !== true ||
   globalThis.__AI_BRIDGE_VIEWPOINT_RUNTIME_V1__?.queueTelemetryReadOnly !== true ||
   globalThis.__AI_BRIDGE_VIEWPOINT_RUNTIME_V1__?.queueTelemetryEphemeral !== true ||
   globalThis.__AI_BRIDGE_VIEWPOINT_RUNTIME_V1__?.queueTelemetryContainsSensitiveIdentity !== false ||
