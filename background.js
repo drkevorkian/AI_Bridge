45eed5de56b471f3c0ce174c8fdcfce275f967fc
drkevorkian <sirus.softs@gmail.com>
2026-09-18 21:14:10 -0600

    Complete D and E delivery cursor hydration

--- background.js
+++ background.js
@@ -868,6 +868,8 @@ async function loadState() {
         A: 0,
         B: 0,
         C: 0,
+        D: 0,
+        E: 0,
         ...(bridgeState.lastDeliveredSeqBySide || {})
       },
       roundStartedAtBySide: { A: null, B: null, C: null, D: null, E: null, ...(bridgeState.roundStartedAtBySide || {}) },
