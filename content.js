aa1adffffb592d98d4914088e14332ed23ceb9f0
drkevorkian <sirus.softs@gmail.com>
2026-09-18 22:30:24 -0600

    Align content handshake with v1.18.0

--- content.js
+++ content.js
@@ -621,7 +621,7 @@
 
   chrome.runtime.onMessage.addListener((msg, _sender, sendResponse) => {
     if (msg.type === "AI_BRIDGE_PING") {
-      sendResponse({ ok: true, host: location.hostname, ready: true, version: "1.16" });
+      sendResponse({ ok: true, host: location.hostname, ready: true, version: "1.18.0" });
       return false;
     }
 
